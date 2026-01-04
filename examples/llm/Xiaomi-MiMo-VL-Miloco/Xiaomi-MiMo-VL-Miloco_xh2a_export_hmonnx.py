08b13d6e4de1b4cef28f1006bdde921f6206f906
Feilong Kong <feilong.kong@houmo.ai>
2026-01-04 11:11:45 +0800

    Xiaomi-MiMo-VL-Miloco
    Change-Id: I4e81d4ee4d1b86ddeae635ffe8c655cfd5cde51f

---
+++ examples/llm/Xiaomi-MiMo-VL-Miloco/Xiaomi-MiMo-VL-Miloco_xh2a_export_hmonnx.py
@@ -0,0 +1,227 @@
+import argparse
+import os.path as osp
+from pathlib import Path
+
+import torch
+from qwen_vl_utils import process_vision_info
+from transformers import AutoProcessor
+from xh_model_zoo.xh_llm import LLMConverter
+from xh_model_zoo.xh_llm.models.qwen2_5_vl import Qwen2_5_VLConvertConfig, VisualConfig
+from xh_model_zoo.xh_llm.models.qwen2_5_vl.modeling_qwen2_5_vl import (
+    Qwen2_5_VLForConditionalGeneration,
+)
+
+from xhquant.api import DeviceType, xhquant_init, QuantScheme, get_root_logger  # isort:skip
+from xh_model_zoo.utils.memory_tracker import MemoryTracker  # isort:skip
+from xh_model_zoo.utils.time_profiler import TimeProfiler  # isort:skip
+import debugpy
+
+
+def demo(model, processor):
+    from accelerate import dispatch_model, infer_auto_device_map
+    from accelerate.utils import get_balanced_memory
+    from xh_model_zoo.xh_llm.quarot import utils
+
+    raw_device = next(model.parameters()).device
+    # model.to(utils.DEV)
+
+    no_split_module_classes = [
+        "LlamaDecoderLayer",
+        "QuantDecoderLayer",
+        "RotateModule",
+        "SmoothModule",
+        "Qwen2DecoderLayer",
+        "Qwen2_5_VLDecoderLayer",
+        "Qwen2_5_VLVisionBlock",
+    ]
+    max_memory = get_balanced_memory(
+        model, no_split_module_classes=no_split_module_classes
+    )
+    device_map = infer_auto_device_map(
+        model, max_memory=max_memory, no_split_module_classes=no_split_module_classes
+    )
+    dispatch_model(
+        model,
+        device_map=device_map,
+        offload_buffers=True,
+        offload_dir="offload",
+        state_dict=model.state_dict(),
+    )
+
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "image",
+                    "image": "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg",
+                },
+                {"type": "text", "text": "Describe this image."},
+            ],
+        }
+    ]
+
+    # Preparation for inference
+    text = processor.apply_chat_template(
+        messages, tokenize=False, add_generation_prompt=True
+    )
+    image_inputs, video_inputs = process_vision_info(messages)
+    inputs = processor(
+        text=[text],
+        images=image_inputs,
+        videos=video_inputs,
+        padding=True,
+        return_tensors="pt",
+    )
+    inputs = inputs.to("cuda")
+
+    # Inference: Generation of the output
+    generated_ids = model.generate(**inputs, max_new_tokens=512)
+    generated_ids_trimmed = [
+        out_ids[len(in_ids) :]
+        for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+    ]
+    output_text = processor.batch_decode(
+        generated_ids_trimmed,
+        skip_special_tokens=True,
+        clean_up_tokenization_spaces=False,
+    )
+    print(output_text)
+    from accelerate.hooks import remove_hook_from_module
+
+    remove_hook_from_module(model)
+    model.to(raw_device)
+    utils.cleanup_memory()
+
+
+def main(args):
+    hf_model_path = osp.normpath(osp.abspath(args.model))
+    model_name = Path(hf_model_path).name
+    target_device = DeviceType.XH2a
+    quant_type = args.quant_type
+
+    ops = dict(
+        MatMul=dict(
+            act_scheme=dict(
+                bits=8,
+                fp_mode="sefp",
+            ),
+            act_schema_2=dict(
+                bits=16,
+                fp_mode="sefp",
+            ),
+        )
+    )
+
+    quant_scheme = QuantScheme(
+        target_device=DeviceType.XH2a, quant_type=quant_type, ops=ops
+    )
+
+    native_model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
+        hf_model_path,
+        torch_dtype=torch.bfloat16,
+        device_map="cpu",
+        # config=config,
+        trust_remote_code=True,
+        attn_implementation="sdpa",
+    )
+
+    if native_model.config.tie_word_embeddings:
+        old_torchscript = native_model.config.torchscript
+        native_model.config.torchscript = True
+        native_model.tie_weights()
+        native_model.config.tie_word_embeddings = False
+        native_model.config.torchscript = old_torchscript
+
+    native_model.eval()
+    native_model.to(torch.bfloat16)
+
+    processor = AutoProcessor.from_pretrained(hf_model_path)
+
+    # demo(native_model, processor)
+    config = Qwen2_5_VLConvertConfig(
+        batch_size=args.batch_size,
+        context_length=args.context_length,
+        quant_scheme=quant_scheme,
+        quant_weight=args.quant_weight,
+        gptqmodel_cfg=args.use_gptqmodel,
+        max_pe_length=args.max_pe_length,
+        visual_config=VisualConfig(
+            image_max_size_h=args.image_max_size_h,
+            image_max_size_w=args.image_max_size_w,
+            image_max_size_t=args.image_max_size_t,
+            temporal_patch_size=args.temporal_patch_size,
+            patch_size=args.patch_size,
+            sample_image_path=args.sample_image_path,
+        ),
+    )
+
+    prefix = f"{model_name}-{target_device}"
+    work_dir = Path("work_dirs") / prefix
+    work_dir.mkdir(exist_ok=True, parents=True)
+    log_file = work_dir / "convert.log"
+    xhquant_init(log_file, debug=args.debug)
+    logger = get_root_logger()
+    with TimeProfiler("convert", logger), MemoryTracker("cuda:0", "convert", logger):
+        LLMConverter.from_pretrained(
+            hf_model_path, "Qwen2_5_VLForConditionalGeneration", config, work_dir
+        )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--debug", action="store_true", help="debug mode")
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="/data01/nfs_shared/llm_models/xiaomi-open-source/Xiaomi-MiMo-VL-Miloco-7B",
+    )
+    parser.add_argument("--batch-size", type=int, default=1, help="batch size")
+    parser.add_argument(
+        "--context-length", type=int, default=2048, help="max sequence length"
+    )
+    parser.add_argument(
+        "--max_pe_length", type=int, default=32768, help="max pe length"
+    )
+    parser.add_argument(
+        "--quant-type", default="w8a8h1_sefp", help="quant type, default is w8a8"
+    )
+    parser.add_argument(
+        "--image_max_size_h", type=int, default=448, help="image max size height"
+    )
+    parser.add_argument(
+        "--image_max_size_w", type=int, default=448, help="image max size width"
+    )
+    parser.add_argument(
+        "--image_max_size_t",
+        type=int,
+        default=2,
+        help="if image, temporal max size is 2, if video, temporal max size is fps",
+    )
+    parser.add_argument("--patch_size", type=int, default=14, help="patch size")
+    parser.add_argument(
+        "--temporal_patch_size", type=int, default=2, help="temporal patch size"
+    )
+    parser.add_argument(
+        "--sample_image_path",
+        type=str,
+        default="data/images/qwen2_vl_demo.jpeg",
+        help="sample image path for generate golden",
+    )
+    parser.add_argument(
+        "--use_gptqmodel", action="store_true", help="use gptqmodel quanted model"
+    )
+    parser.add_argument(
+        "--quant_weight",
+        type=str,
+        default=None,
+        help="quant weight path, for example: gptq or quarot, if empty, use w8a8",
+    )
+    args = parser.parse_args()
+
+    debugpy.listen(("0.0.0.0", 2345))
+    print("✅ debugpy listening on 0.0.0.0:2345, waiting for VSCode attach...")
+    debugpy.wait_for_client()
+    print("✅ VSCode attached, continue running.")
+
+    main(args)
