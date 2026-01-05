18cc79e4e83edc4d26ea17db157af244675a3073
xing.hu <xing.hu.gg@gmail.com>
2026-01-05 16:46:25 +0800

    add gptq_quant.py
    Change-Id: I2f4e298633043461adb22e347a29a66c36370200

--- examples_new/llm/qwen3moe/qwen3_moe_quant.py
+++ examples_new/llm/qwen3moe/qwen3_moe_quant.py
@@ -1,1 +1,107 @@
+import argparse
+import os
+from pathlib import Path
+
+import torch
+from datasets import load_dataset
+from transformers import AutoTokenizer
+
+from gptqmodel import GPTQModel, QuantizeConfig
+
+
+def build_calib_dataset(tokenizer, nsamples: int = 128, seqlen: int = 2048):
+    texts = load_dataset("wikitext", "wikitext-2-raw-v1", split="train").select(range(nsamples))["text"]
+    dataset = []
+    for t in texts:
+        enc = tokenizer(t, truncation=True, max_length=seqlen, return_tensors="pt")
+        dataset.append(
+            {
+                "input_ids": enc["input_ids"],
+                "attention_mask": enc["attention_mask"],
+            }
+        )
+    return dataset
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="/data01/datasets/Qwen2-7B",
+        help="HF 模型 ID 或本地路径，例如 mistralai/GptOss-8x7B-v0.1",
+    )
+    parser.add_argument("--output-dir", type=str, default=None)
+    # Calibration Dataset
+    parser.add_argument("--num-samples", type=int, default=256)
+    parser.add_argument("--calibration-file", type=str, default=None)
+    # Quantization Setting
+    parser.add_argument("--bits", type=int, default=4)
+    parser.add_argument("--group-size", type=int, default=64)
+    parser.add_argument("--batch-size", type=int, default=1)
+    parser.add_argument("--nsamples", type=int, default=128)
+    parser.add_argument("--seqlen", type=int, default=2048)
+    parser.add_argument("--rotation", type=str, default=None, choices=[None, "hadamard"], help="可选旋转优化")
+    parser.add_argument("--sym", default=True, help="对称量化")
+    parser.add_argument("--mse", default=False, help="开启 MSE 辅助优化", action="store_true")
+    parser.add_argument("--hessian-mse", default=False, help="开启 Hessian MSE 辅助优化", action="store_true")
+    # Weather demo
+    parser.add_argument("--demo", action="store_true", help="简单推理验证")
+    parser.add_argument("--demo-prompt", type=str, default="中国是一个", help="简单推理验证的提示词")
+    args = parser.parse_args()
+
+    model_id = os.path.normpath(args.model)
+    model_name = Path(model_id).name
+
+    save_dir = args.output_dir or f"work_dirs/{model_name}-{args.bits}bit-{args.group_size}group"
+
+    tokenizer = AutoTokenizer.from_pretrained(model_id, use_fast=True, trust_remote_code=True)
+
+    qcfg = QuantizeConfig(
+        bits=args.bits,
+        group_size=args.group_size,
+        mse=bool(args.mse),
+        rotation=args.rotation,
+        hessian_mse=args.hessian_mse,
+        device="cuda",
+        offload_to_disk=False,
+    )
+
+    model = GPTQModel.load(
+        model_id,
+        qcfg,
+        trust_remote_code=True,
+        # device_map={"": "cuda:0"}  # 所有模块都在 GPU 0 上，关闭 tensor 并行
+    )
+
+    # 根据显存调整 batch_size 以加速量化
+    if args.calibration_file is None:
+        calib_dataset = build_calib_dataset(tokenizer, nsamples=args.num_samples, seqlen=args.seqlen)
+    else:
+        # args.calibration_file 一定要以 .jsonl 结尾
+        import json
+
+        assert args.calibration_file.endswith(".jsonl"), "calibration_file must be a .jsonl file"
+        calib_dataset = list()
+        with open(args.calibration_file, encoding="utf-8") as file:
+            for i, line in enumerate(file):
+                if i < args.nsamples:
+                    calib_dataset.append(json.loads(line)["text"])
+                else:
+                    break
+
+    model.quantize(calib_dataset, batch_size=args.batch_size)
+
+    model.save(save_dir)
+
+    # 简单推理验证
+    if args.demo:
+        device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        model = GPTQModel.load(save_dir, device=device, trust_remote_code=True)
+        prompt = args.demo_prompt
+        print(tokenizer.decode(model.generate(**tokenizer(prompt, return_tensors="pt").to(device))[0]))
+
+
+if __name__ == "__main__":
+    main()
  
