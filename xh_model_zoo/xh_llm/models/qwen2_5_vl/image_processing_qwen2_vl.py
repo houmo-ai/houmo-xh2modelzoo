8a81a1e7781a5cd965d7b2694f86bfb2e81ad2db
xing.hu <xing.hu.gg@gmail.com>
2026-01-27 19:55:51 +0800

    Add copyright for modifications to open source files
    Change-Id: Ifd53819dc0f18d046b85f9a5f81b7aaa16106d5b

--- xh_model_zoo/xh_llm/models/qwen2_5_vl/image_processing_qwen2_vl.py
+++ xh_model_zoo/xh_llm/models/qwen2_5_vl/image_processing_qwen2_vl.py
@@ -1,22 +1,21 @@
-# Copyright 2025 HOUMO AI
+# coding=utf-8
+# Copyright 2025 The Qwen Team and The HuggingFace Inc. team. All rights reserved.
+# Copyright 2025 HOUMO AI. All rights reserved.
 #
-# File: image_processing_qwen2_vl.py
-# Description:
-#   Image Processing Qwen2 Vl implementation.
+# Modifications:
+# - Portions of this file have been modified by HOUMO AI.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     https://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# SPDX-License-Identifier: Apache-2.0
 
 
 import math
