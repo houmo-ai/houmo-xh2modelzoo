cb45063f6852b73c3ee47962006d9e2bab80ccf5
许晨 <chen.xu@houmo.ai>
2026-04-14 14:23:15 +0800

    开源协议检查 HMSW-3934 HMSW-3962 HMSW-3965 HMSW-3960
    Change-Id: I814a09a00a96df1fe46fba5c807a03aafa2d5e5b

--- examples/cv/yolo/yolov8_without_postprocess/yolov8m_without_postprocess_hmonnx_test.py
+++ examples/cv/yolo/yolov8_without_postprocess/yolov8m_without_postprocess_hmonnx_test.py
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# SPDX-License-Identifier: Apache-2.0
+# SPDX-License-Identifier: MIT
 
 import argparse
 from pathlib import Path
